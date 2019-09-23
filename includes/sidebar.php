90de1580744328ee0facfd662b919739957411be
swopnil <swopnil420@gmail.com>
2019-09-23 06:29:47 +0600

    Display the categories to sidebar.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,42 +18,36 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+                    <?php
+
+                        // Select all data from categoried
+                        $query = "SELECT * FROM categories";
+                        // Connect data for getting data from categories
+                        $select_all_categories_query = mysqli_query($connect, $query);
+
+                    ?>
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php
+
+                                    // Fetch the category from categories table by associative array
+                                    while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                                        $cat_title = $row["cat_title"];
+
+                                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                                    }                                
+                                
+                                ?>                                
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
                     </div>
                     <!-- /.row -->
                 </div>
 
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php 
+                    include("includes/widget.php");
+                ?>
 
             </div>
