b888e60b7b7443991e4acac9ef11bcc3d27a6127
swopnil <swopnil420@gmail.com>
2019-09-28 22:53:27 +0600

    Make the relation with the comment and post

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -3,7 +3,6 @@
         <tr>
             <th>ID</th>
             <th>Author</th>
-            <th>Post Name</th>
             <th>Comment</th>
             <th>Email</th>
             <th>Status</th>
@@ -43,15 +42,24 @@
 //                    $cat_title = $row['cat_title'];
 //                }
 
+                // Get the post title where commenting
+                $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
+                $select_post_id = mysqli_query($connect, $query);
+
+                // Fetch the post where commenting
+                while ($rows = mysqli_fetch_assoc($select_post_id)) {
+                    $post_id = $rows['post_id'];
+                    $post_title = $rows['post_title'];
+                }
+
                 echo "
                     <tr>
                         <td>{$comment_id}</td>
                         <td>{$comment_author}</td>
-                        <td>{$comment_post_id}</td>
                         <td>{$comment_content}</td>
                         <td>{$comment_email}</td>
                         <td>{$comment_status}</td>
-                        <td>Some Text</td>
+                        <td><a href='../post.php?p_id={$post_id}'>{$post_title}</a></td>
                         <td>{$comment_date}</td>
                         <td><a href='#'>Approved</a></td>
                         <td><a href='#'>Unapproved</a></td>                        
