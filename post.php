d4e37f739511343fb4457d9357c85758634ec9ef
swopnil <swopnil420@gmail.com>
2019-09-29 06:26:08 +0600

    Displaying comment based on approval

--- post.php
+++ post.php
@@ -110,18 +110,38 @@ include("includes/navigation.php");
 
             <!-- Posted Comments -->
 
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-            </div>
+            <?php
+
+                // Get data from DB by this query
+                $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} AND comment_status = 'approved' ORDER BY comment_id DESC ";
+                $select_comment_query = mysqli_query($connect, $query);
+
+                // Check the DB is connected or not
+                if (!$select_comment_query) {
+                    die("Query Faield!" . mysqli_error($connect));
+                }
+
+                while($row = mysqli_fetch_array($select_comment_query)) {
+                    $comment_date = $row['comment_date'];
+                    $comment_content = $row['comment_content'];
+                    $comment_author = $row['comment_author'];
+                    ?>
+
+                    <!-- Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><?php echo $comment_author; ?>
+                                <small><?php echo $comment_date; ?></small>
+                            </h4>
+                            <?php echo $comment_content; ?>
+                        </div>
+                    </div>
+
+                <?php }
+            ?>
         </div>
 
         <!-- Siderbar -->
