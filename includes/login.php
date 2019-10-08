cce1cd2994796e66b47244b40eed39810e5bd61d
swopnil <swopnil420@gmail.com>
2019-10-08 21:09:36 +0600

    Update the project due to password encryption

--- includes/login.php
+++ includes/login.php
@@ -12,7 +12,7 @@
         $username = mysqli_real_escape_string($connect, $username);
         $password = mysqli_real_escape_string($connect, $password);
 
-        $query = "SELECT * FROM users WHERE username = '{$username}' AND user_password = '{$password}' ";
+        $query = "SELECT * FROM users WHERE username = '{$username}' ";
         $select_user_query = mysqli_query($connect, $query);
 
         if (!$select_user_query) {
@@ -28,11 +28,26 @@
             $db_user_role = $row['user_role'];
         }
 
+        // Access the rand salt column
+        $query = "SELECT user_randSalt FROM users";
+        $select_randSalt_query = mysqli_query($connect, $query);
+
+        if (!$select_randSalt_query) {
+            die("Query Failed!" . mysqli_error($connect));
+        }
+
+        while($row = mysqli_fetch_array($select_randSalt_query)) {
+            $salt = $row['user_randSalt'];
+        }
+
+        // Encrypting the password
+        $password = crypt($password, $salt);
+
         // Check the username and password is correct or not
         if ($username !== $db_username && $password !== $db_user_password) {
             header("Location: ../index.php");
         } else {
-
+            if (session_status() == PHP_SESSION_NONE) session_start();
             $_SESSION['username'] = $db_username;
             $_SESSION['firstname'] = $db_user_firstname;
             $_SESSION['lastname'] = $db_user_lastname;
--- registration.php
+++ registration.php
@@ -8,18 +8,14 @@ if (isset($_POST['submit'])) {
 
     // Catch the register form fields
     $username = $_POST['username'];
-    $firstName = $_POST['firstname'];
-    $lastName = $_POST['lastname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     // Fields validation
-    if (!empty($username) && !empty($firstName) && !empty($lastName) && !empty($email) && !empty($password)) {
+    if (!empty($username) && !empty($email) && !empty($password)) {
 
         // Escaping the unknown string
         $username = mysqli_real_escape_string($connect, $username);
-        $firstName = mysqli_real_escape_string($connect, $firstName);
-        $lastName = mysqli_real_escape_string($connect, $lastName);
         $email = mysqli_real_escape_string($connect, $email);
         $password = mysqli_real_escape_string($connect, $password);
 
@@ -38,10 +34,10 @@ if (isset($_POST['submit'])) {
         $password = crypt($password, $salt);
 
         // Insert register user into the user table
-        $query = "INSERT INTO users (username, user_firstname, user_lastname, user_email, user_password, user_image, user_role) VALUES ('{$username}', '{$firstName}', '{$lastName}', '{$email}', '{$password}', '', 'subscriber' )";
+        $query = "INSERT INTO users (username, user_email, user_password, user_role) VALUES ('{$username}', '{$email}', '{$password}', 'subscriber' )";
         $register_user_query = mysqli_query($connect, $query);
         if (!$register_user_query) {
-            die("Query Failed!" . mysqli_error($connect));
+            die("Query Failed! " . mysqli_error($connect));
         }
 
         $message = "<p class='alert-success text-center'>Registration has been submitted!</p>";
@@ -69,14 +65,6 @@ if (isset($_POST['submit'])) {
                             <div class="form-group">
                                 <label for="username" class="sr-only">username</label>
                                 <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username">
-                            </div>
-                            <div class="form-group">
-                                <label for="firstname" class="sr-only">First Name</label>
-                                <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Enter First Name">
-                            </div>
-                            <div class="form-group">
-                                <label for="lastname" class="sr-only">Last Name</label>
-                                <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Enter Last Name">
                             </div>
                              <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
