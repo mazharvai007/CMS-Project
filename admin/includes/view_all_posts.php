e7ce8a3011bff6570cd8ccc09734f62652707354
swopnil <swopnil420@gmail.com>
2019-10-11 07:04:44 +0600

    added post clone/duplicate

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -22,6 +22,43 @@
                    $update_to_draft = mysqli_query($connect, $query);
                    confirmQuery($update_to_draft);
                    break;
+               case 'duplicate':
+                   // Detailed way
+//                   $query = "SELECT * FROM posts WHERE post_id = {$selectPostValueAsID} ";
+//                   $select_duplicate_post_query = mysqli_query($connect, $query);
+//
+                   // Fetch the query
+//                   while ($row = mysqli_fetch_array($select_duplicate_post_query)) {
+//                       $post_title = $row['post_title'];
+//                       $post_category_id = $row['post_category_id'];
+//                       $post_date = $row['post_date'];
+//                       $post_author = $row['post_author'];
+//                       $post_status = $row['post_status'];
+//                       $post_image = $row['post_image'];
+//                       $post_tags = $row['post_tags'];
+//                       $post_content = $row['post_content'];
+//                   }
+//
+                   // Insert duplicate post into db
+//                   $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}')";
+//                   $copy_the_post_query = mysqli_query($connect, $query);
+
+                   // Check the duplicate query
+//                   if (!$copy_the_post_query) {
+//                       die("Query Failed! " . mysqli_error($connect));
+//                   }
+//
+//                   confirmQuery($copy_the_post_query);
+
+                   // Alternate way
+                   $query = "INSERT INTO posts (post_tags, post_status, post_category_id, post_title, post_author, post_date ,post_image, post_content )";
+                   $query .= "SELECT post_tags, post_status, post_category_id, post_title, post_author, now() ,post_image, post_content FROM posts WHERE post_id='{$selectPostValueAsID}'";
+                   $alt_copy_the_post_query = mysqli_query($connect, $query);
+
+                   confirmQuery($alt_copy_the_post_query);
+
+                   echo "<p class='bg-success'>Post duplicate successfully!</p>";
+                   break;
                case 'delete':
                    $query = "DELETE FROM posts WHERE post_id = {$selectPostValueAsID} ";
                    $update_to_delete = mysqli_query($connect, $query);
@@ -48,6 +85,7 @@
                         <option value="published">Published</option>
                         <option value="unpublished">Unpublished</option>
                         <option value="draft">Draft</option>
+                        <option value="duplicate">Duplicate</option>
                         <option value="delete">Delete</option>
                     </select>
                 </div>
@@ -83,7 +121,7 @@
 
                     <?php
                         // Select all data from categories
-                        $query = "SELECT * FROM posts";
+                        $query = "SELECT * FROM posts ORDER BY post_id DESC";
                         // Connect DB for getting data from categories
                         $get_posts = mysqli_query($connect, $query);
 
