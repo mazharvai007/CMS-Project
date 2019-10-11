023e7f756df26a67787f11f46fcc7c687f71899a
swopnil <swopnil420@gmail.com>
2019-10-11 07:26:15 +0600

    Added preloader in admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -39,6 +39,9 @@
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <!-- Preloader css-->
+<!--    <link rel="stylesheet" href="../admin/css/loader.css">-->
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
--- admin/js/script.js
+++ admin/js/script.js
@@ -18,5 +18,12 @@ $(document).ready(function() {
             });
         }
         e.preventDefault();
-    })
+    });
+
+    // Page loader
+    // var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+    // $("body").prepend(div_box);
+    // $('#load-screen').delay(700).fadeOut(600, function () {
+    //     $(this).remove();
+    // });
 });
