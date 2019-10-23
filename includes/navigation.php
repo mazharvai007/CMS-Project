5bfac33894d5d82ad86bcb208a6e320ba5be91a9
swopnil <swopnil420@gmail.com>
2019-10-24 00:10:02 +0600

    Rewrite post page and query string

--- includes/navigation.php
+++ includes/navigation.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">CMS Project</a>
+                <a class="navbar-brand" href="../CMS-Project">CMS Project</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
