beb319a9f1728900192d01570ee672f27a7f303b
swopnil <swopnil420@gmail.com>
2019-11-01 16:22:17 +0600

    Successfully complete the multilingual on registration page.

--- registration.php
+++ registration.php
@@ -6,22 +6,22 @@ include("includes/navigation.php");
 require "./vendor/autoload.php";
 
 // Language settings
-if (isset($_GET['lang'])) {
+if (isset($_GET['lang']) && !empty($_GET['lang'])) {
     $_SESSION['lang'] = $_GET['lang'];
 
-    if (isset($_SESSION['lang']) && $_SESSION['lang'] == $_GET['lang']) {
+    if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
         echo "
             <script type='text/javascript'>
                 location.reload();
             </script>
         ";
     }
+}
 
-    if (isset($_SESSION['lang'])) {
-        include "includes/languages".$_SESSION['lang']."php";
-    } else {
-        include "includes/languages/en.php";
-    }
+if (isset($_SESSION['lang'])) {
+    include ("includes/languages/" . $_SESSION['lang'].".php");
+} else {
+    include ("includes/languages/en.php");
 }
 
 // Start pusher
@@ -100,8 +100,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <form id="lang_form" class="navbar-form navbar-right" action="" method="get">
             <div class="form-group">
                 <select name="lang" class="form-control" onchange="changeLang()">
-                    <option value="en">English</option>
-                    <option value="es">Spanish</option>
+                    <option value="en" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'en') {echo "selected";} ?>>English</option>
+                    <option value="es" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'es') {echo "selected";} ?>>Spanish</option>
                 </select>
             </div>
         </form>
@@ -112,25 +112,25 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
-                    <h1>Register</h1>
+                    <h1><?php echo _REGISTER; ?></h1>
                         <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username" autocomplete="on" value="<?php echo isset($username) ? $username : ''; ?>">
+                                <input type="text" name="username" id="username" class="form-control" placeholder="<?php echo _USERNAME; ?>" autocomplete="on" value="<?php echo isset($username) ? $username : ''; ?>">
                                 <p><?php echo isset($error['username']) ? '<span class="alert-danger">' . $error['username'] . '</span>' : '' ?></p>
                             </div>
                              <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" autocomplete="on" value="<?php echo isset($email) ? $email : ''; ?>">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="<?php echo _EMAIL; ?>" autocomplete="on" value="<?php echo isset($email) ? $email : ''; ?>">
                                  <p><?php echo isset($error['email']) ? '<span class="alert-danger">' . $error['email'] . '</span>' : '' ?></p>
                             </div>
                              <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <input type="password" name="password" id="key" class="form-control" placeholder="<?php echo _PASSWORD; ?>">
                                  <p><?php echo isset($error['password']) ? '<span class="alert-danger">' . $error['password'] . '</span>' : '' ?></p>
                             </div>
 
-                            <input type="submit" name="register" id="btn-login" class="btn btn-primary btn-lg btn-block" value="Register">
+                            <input type="submit" name="register" id="btn-login" class="btn btn-primary btn-lg btn-block" value="<?php echo _REGISTER; ?>">
                         </form>
 
                     </div>
